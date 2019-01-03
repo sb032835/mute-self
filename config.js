4f0307f7172acbe6d1463dc923ce09b2411e86a3
bobby <sb032835@msn.com>
2019-01-04 06:49:41 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    channels: [3, 27]
-    /*
-    # say = 0, party = 1, guild = 2, area = 3, trade = 4, greet = 9,
-    # private = 11-18, p-notice = 21, emote = 26, global = 27, r-notice = 25,
-    # raid = 32, megaphone = 213, guild-adv = 214
-    */
-}
